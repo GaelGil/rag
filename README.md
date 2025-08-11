2da339f3556dd28fd2136fdbcbce10ef66f97119
Gael <gaelgilbello27@gmail.com>
2025-08-11 12:39:50 -0700

    removing old stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# rag
-
-A simple rag (Retrival Augmented Generation) implementation
--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Answer(BaseModel):
-    text: str = Field(description="The answer to the user query as text")
-    tool_used: list = Field(description="The list of tools used to get this answer")
-
-
-# class
